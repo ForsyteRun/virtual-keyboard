ff60d6a73716f1327c3af3407d4f9cbb978bf194
ForsyteRun <forsyterun@gmail.com>
2023-04-23 13:36:25 +0300

    feat: correct layout

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 // import './index.html';
-// import './sass/main.scss';
+import './sass/main.scss';
+
+const generateTextArea = () => {
+  const body = document.querySelector('body');
+
+  const wrapper = document.createElement('div');
+  wrapper.className = 'wrapper';
+
+  const textArea = document.createElement('textarea');
+  textArea.className = 'input input__container';
+
+  body.append(wrapper);
+  wrapper.append(textArea);
+};
+
 window.onload = () => {
-  console.log(111);
+  generateTextArea();
 };
