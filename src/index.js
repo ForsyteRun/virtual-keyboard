40079ec17db3be1d9063c4d2547b0fb8f0bed731
ForsyteRun <forsyterun@gmail.com>
2023-04-24 20:53:28 +0300

    feat: add arrows svg

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 import './sass/main.scss';
 
 const data = {
-  en: ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowUp', 'ArrowDown', 'ArrowRight'],
-  ru: ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Control', 'AltGraph', 'Control', 'ArrowLeft', 'ArrowUp', 'ArrowDown', 'ArrowRight'],
+  en: ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'Control'],
+  ru: ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', 'ArrowUp', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'AltGraph', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'Control'],
 };
 
 const generateLayout = () => {
@@ -41,12 +41,48 @@ const generateKeys = (lang) => {
 const setKeySize = () => {
   const backColons = document.querySelector('.key0');
   const backspace = document.querySelector('.key13');
-  backColons.classList.add('key_short');
-  backspace.classList.add('key_middle');
+  const tab = document.querySelector('.key14');
+  const slash = document.querySelector('.key27');
+  const caps = document.querySelector('.key28');
+  const enter = document.querySelector('.key40');
+  const shiftLeft = document.querySelector('.key41');
+  const shifRight = document.querySelector('.key53');
+  const ctrlRight = document.querySelector('.key62');
+  const space = document.querySelector('.key57');
+  backColons.classList.add('key_first-row');
+  slash.classList.add('key_second-row');
+  tab.classList.add('key_second-row');
+  backspace.classList.add('key_back-space');
+  caps.classList.add('key_therd-row');
+  enter.classList.add('key_therd-row');
+  shiftLeft.classList.add('key_forth-row');
+  space.classList.add('key_space');
+  shifRight.classList.add('key_right-size');
+  ctrlRight.classList.add('key_right-size');
+};
+
+const serArrowSvg = () => {
+  const arrowUp = document.querySelector('.key52 span');
+  const arrowDown = document.querySelector('.key60 span');
+  const arrowLeft = document.querySelector('.key59 span');
+  const arrowRight = document.querySelector('.key61 span');
+  arrowUp.textContent = '';
+  arrowDown.textContent = '';
+  arrowLeft.textContent = '';
+  arrowRight.textContent = '';
+  arrowUp.classList.add('arrow__up');
+  arrowUp.classList.add('arrow');
+  arrowDown.classList.add('arrow__down');
+  arrowDown.classList.add('arrow');
+  arrowLeft.classList.add('arrow__left');
+  arrowLeft.classList.add('arrow');
+  arrowRight.classList.add('arrow__right');
+  arrowRight.classList.add('arrow');
 };
 
 window.onload = () => {
   generateLayout();
   generateKeys('en');
   setKeySize();
+  serArrowSvg();
 };
